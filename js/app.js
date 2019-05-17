c1ed85036a4f5507ab40bae76f5edd49e27bde0f
lionklass <geo3dgraff@yandex.ru>
2019-05-17 13:03:02 +0300

    Changing before news comments

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+const my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+]
+
 const Comments = React.createClass ({
     render: () => {
         return (
@@ -23,7 +38,7 @@ const App = React.createClass ({
         return (
             <div className = 'app'>
                 Всем привет, я компонент App! Я умею отображать новости
-                <News/>
+                <News data={my_news}/> {/* добавили свойство data */}
                 <Comments/>
             </div>
         )
