8ac3fa05a9ece631bef6cc56685754cf4cff65f8
lionklass <geo3dgraff@yandex.ru>
2019-05-20 02:36:10 +0300

    component into component

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
+const App = () => {
+    return <p>Привет Я компонент App</p>
+}
+
+class BigApp extends React.Component {
+    render () {
+        return (
+            <div>
+            <h1>Привет Я компонент BigApp!</h1>
+            <p className = 'red'>Компоненты можно вкладывать друг в друга</p>
+            <App/>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello World!</h1>,
+    <BigApp/>,
     document.getElementById('root')
 )
